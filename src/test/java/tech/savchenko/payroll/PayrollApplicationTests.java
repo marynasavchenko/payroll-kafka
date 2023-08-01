027ceb18897d238d8395986009e2e9f457a0449a
marynasavchenko <maryna.savchenko@openvalue.de>
2023-08-01 16:47:53 +0200

    app skeleton

---
+++ src/test/java/tech/savchenko/payroll/PayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.savchenko.payroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
